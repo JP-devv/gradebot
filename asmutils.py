e479a7e899235cf743024371699c1b0fc127339f
Johann <johann.plaster@icloud.com>
2023-03-20 01:47:45 -0500

    upgraded asmutils, now featuring a more comprehensive instructions extractor

--- asmutils.py
+++ asmutils.py
@@ -1,4 +1,4 @@
-#Helpful utilities when grading ASM
+# Helpful utilities when grading ASM
 
 import os
 
@@ -33,6 +33,28 @@ def getMem(file):
     if elem[2] == '0' or elem[2] == '?':
       new_data.append(elem)
   return new_data
+
+# Get instructions from code section
+def getIns(file):
+  # Store data in a list
+  data = []
+  with open(file) as f:
+    lines = f.readlines()
+    # Only get code between .data and .code
+    is_past_code = False
+    for line in lines:
+      line = line.casefold()
+      if not is_past_code and '.code' in line:
+        is_past_code = True
+        continue
+      elif is_past_code and 'invoke' in line:
+        is_past_code = False
+        break
+      elif is_past_code and line[0] != ';' and line[0] != '\n':
+        bits = line.replace(';', ' ').replace(',','').split()
+        if len(bits) > 2:
+          data.append(bits[:3])
+    return data
     
       
 # Determine if 32 bit or 64 bit program
@@ -42,23 +64,6 @@ def is32(file):
     for line in lines:
       if '.386' in line:
         return True
-    return False
-
-# Determine if a certain command is used
-def usesCommand(file, command):
-  command = command.casefold()
-  with open(file) as f:
-    lines = f.readlines()
-    # Only parse after flag checkpoint
-    flag = False
-    for line in lines:
-      line = line.casefold()
-      if not flag and '.code'.casefold() in line:
-        flag = True
-        continue
-      elif flag:
-       if command in line:
-         return True
     return False
       
 # Gets function names
@@ -72,24 +77,6 @@ def getFunction(file):
         names.append(line.split()[0])
     return names
 
-# Assert functionality
-def assertt(msg, expected, outcome):
-  # Convert to string just in case
-  expected = str(expected)
-  outcome = ''.join(str(outcome))
-  if expected.casefold() in outcome.casefold():
-    print(f'{msg}: PASS')
-  else:
-    print(f'{msg}: FAIL')
-
-# Assert functionality
-def assertt(msg, test):
-  if test: 
-    print(f'{msg}: PASS')
-  else:
-    print(f'{msg}: FAIL')
-
-
 # Standardize linker
 def standardize(file):
   os.system(f'sed -i "" "s/^main/_main/" "{file}"')
@@ -98,4 +85,4 @@ def standardize(file):
 # Copy file to clipboard and delete
 def replace(file):
   os.system(f'cat "{file}" | pbcopy && rm "{file}"')
-  print(f'Copied {file}')
+  print(f'Replaced {file} successfully')
